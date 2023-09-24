15c17dfd060ca0d7be0606176eb06d4e0873d13e
Tom Donohue <monodot@gmail.com>
2023-09-24 14:29:49 +0100

    Remove dropped component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DeploymentView from '../views/DeploymentView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,12 +16,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/deployment',
-      name: 'deployment',
-      component: DeploymentView
-      // component: () => import('../views/DeploymentView.vue')
     }
   ]
 })
