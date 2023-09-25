fa6dd3b2b2a16037833f8e2059268ad11618448f
Tom Donohue <monodot@gmail.com>
2023-09-25 23:23:00 +0100

    Add Faro

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  define: {
+    'process.env.FARO_API_URL': process.env.FARO_API_URL !== undefined ? JSON.stringify(process.env.FARO_API_URL) : JSON.stringify('http://localhost:4317'),
+    'process.env.NODE_ENV': process.env.NODE_ENV !== undefined ? JSON.stringify(process.env.NODE_ENV) : JSON.stringify('development'),
   }
 })
