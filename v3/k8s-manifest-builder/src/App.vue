748f61b8f8210873ae155a97eb174927e26746f6
Tom Donohue <monodot@gmail.com>
2024-12-27 17:15:52 +0000

    feat: add shadcn-vue and Button component

--- v3/k8s-manifest-builder/src/App.vue
+++ v3/k8s-manifest-builder/src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { Button } from '@/components/ui/button';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <Button>Click me</Button>
   </main>
 </template>
 
