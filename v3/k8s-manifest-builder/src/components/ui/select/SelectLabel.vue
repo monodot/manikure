3295f5248a53124649f09a46f4ce836ec832d402
Tom Donohue <monodot@gmail.com>
2024-12-28 11:12:33 +0000

    Add ui components

---
+++ v3/k8s-manifest-builder/src/components/ui/select/SelectLabel.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { cn } from '@/lib/utils';
+import { SelectLabel } from 'radix-vue';
+
+const props = defineProps({
+  for: { type: String, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+</script>
+
+<template>
+  <SelectLabel
+    :class="cn('py-1.5 pl-8 pr-2 text-sm font-semibold', props.class)"
+  >
+    <slot />
+  </SelectLabel>
+</template>
---
+++ v3/k8s-manifest-builder/src/components/ui/select/SelectTrigger.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { cn } from '@/lib/utils';
+import { ChevronDown } from 'lucide-vue-next';
+import { SelectIcon, SelectTrigger, useForwardProps } from 'radix-vue';
+import { computed } from 'vue';
+
+const props = defineProps({
+  disabled: { type: Boolean, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <SelectTrigger
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:truncate text-start',
+        props.class,
+      )
+    "
+  >
+    <slot />
+    <SelectIcon as-child>
+      <ChevronDown class="w-4 h-4 opacity-50 shrink-0" />
+    </SelectIcon>
+  </SelectTrigger>
+</template>
