885fe86e2a2630e0e11b3f5924ed0bf5b1da3d8d
Tom Donohue <monodot@gmail.com>
2025-01-09 20:14:38 +0000

    Reformat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,14 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import {type ClassValue, clsx} from 'clsx'
+import {twMerge} from 'tailwind-merge'
 import type {Resource} from "@/types/resource.ts";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
 
 export function generateId(resources: Resource[]) {
-  const nextId = resources.reduce((max, resource) => Math.max(max, resource.id ?? 0), 0) + 1;
-  return nextId;
+    const nextId = resources.reduce((max, resource) => Math.max(max, resource.id ?? 0), 0) + 1;
+    return nextId;
 }
 
 export function cleanupEmptyValues(obj: any): any {
